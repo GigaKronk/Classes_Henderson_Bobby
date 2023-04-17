730a86a533de2779383fdc7d3d1d1e60e5744c9b
bobby henderson <bobby.henderson@gctcok.edu>
2023-04-17 09:04:47 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes_Henderson_Bobby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Greetings greetings = new Greetings();
+            //
+            greetings.Welcome();
+
+            //
+            Console.WriteLine("waht si your name squire");
+            string userName = Console.ReadLine();
+            //
+            greetings.Hello(userName);
+
+        }
+    }
+}
